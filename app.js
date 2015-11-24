d8d5d6bf8b594a87b1a1ff667ceaaadd179b3b1f
John-Lin <linton.tw@gmail.com>
2015-11-24 16:57:25 +0800

    hostname

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let bodyParser = require('body-parser');
 // Database
 let redis = require('redis');
 const RDS_PORT = 6379;
-const RDS_HOST = 'b53fbce6e927';
+const RDS_HOST = 'HSNLredis';
 const RDS_OPTS = {};
 let client = redis.createClient(RDS_PORT, RDS_HOST, RDS_OPTS);
 
